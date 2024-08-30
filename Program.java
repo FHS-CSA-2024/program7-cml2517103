8c69200dd7714d7d1d236b9c4b9dfb3943127d81
cml2517103 <cml2517103@gmail.com>
2024-08-30 09:01:14 -0700

    Update Program7.java

--- Program.java
+++ Program.java
@@ -1,9 +1,24 @@
-//import stuff here
+import java.util.Scanner;
 
-//Your code here
-
-//Paste console output below:
-/*
-
-
-*/
+public class Program7{
+    public static void main(String[] args){
+        Scanner survey = new Scanner(System.in);
+        
+        System.out.print("Enter schrute-bucks: ");
+        int sBucks = survey.nextInt();
+        System.out.print("Enter klevins: ");
+        int klevins = survey.nextInt();
+        System.out.print("Enter stanley-nickels: ");
+        double sNickels = survey.nextDouble();
+        
+        final int KperSB = 20;
+        final int SNperK = 12;
+        final int SNperSB = 100;
+        
+        sNickels += (klevins * ((double)SNperK));
+        sNickels *= ((double)SNperSB / (SNperK * KperSB)); 
+        double decimalSBucks = (sNickels / 100.0) + sBucks;
+        
+        System.out.printf("Decimal schrute-bucks = $%.2f", decimalSBucks);
+    }
+}
